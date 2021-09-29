338fccfb0685d29619fc3bbdb1cf570f554a4fe7
Akshat Jain <aki.jain51@gmail.com>
2021-09-29 18:46:47 +0530

    added components to home screen

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jiit_shiksha/utils/subjectTile.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,11 +9,146 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String name = "Akshat";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("JIIT Shiksha"),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hi $name",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "What would you like to learn today",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                maxLines: 1,
+                decoration: InputDecoration(
+                  hintText: "Search Subjects",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Colors.grey[600]!,
+                    ),
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Colors.grey[600]!,
+                    ),
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          SubjectTile(
+                            subject: "Maths",
+                          ),
+                          SubjectTile(
+                            subject: "DBMS",
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SubjectTile(
+                            subject: "Maths",
+                          ),
+                          SubjectTile(
+                            subject: "DBMS",
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SubjectTile(
+                            subject: "Maths",
+                          ),
+                          SubjectTile(
+                            subject: "DBMS",
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SubjectTile(
+                            subject: "Maths",
+                          ),
+                          SubjectTile(
+                            subject: "DBMS",
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SubjectTile(
+                            subject: "Maths",
+                          ),
+                          SubjectTile(
+                            subject: "DBMS",
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SubjectTile(
+                            subject: "Maths",
+                          ),
+                          SubjectTile(
+                            subject: "DBMS",
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Color(0xFFFAF1FF),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: "Subjects",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "Search",
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/utils/subjectTile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SubjectTile extends StatelessWidget {
+  final String subject;
+  const SubjectTile({
+    Key? key,
+    this.subject = "Maths",
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: 100,
+        margin: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Color(0xFFB352DD),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            subject,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
